ebb34e1a65d89e5a19d03710593e82ecfada6084
Andy Cockroft <acockroft1994@gmail.com>
2023-07-18 11:43:39 +0200

    token auth complete

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ require("./db");
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
 const express = require("express");
-
+const { isAuthenticated } = require("./middleware/jwt.middleware");
 const app = express();
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
@@ -19,10 +19,13 @@ const allRoutes = require("./routes/index.routes");
 app.use("/api", allRoutes);
 
 const projectRouter = require("./routes/project.routes");
-app.use("/api", projectRouter);
+app.use("/api", isAuthenticated, projectRouter);
 
-const taskRouter = require("./routes/task.routes"); // <== IMPORT
-app.use("/api", taskRouter);
+const taskRouter = require("./routes/task.routes");
+app.use("/api", isAuthenticated, taskRouter);
+
+const authRouter = require("./routes/auth.routes");
+app.use("/auth", authRouter);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
--- models/User.model.js
+++ models/User.model.js
@@ -5,19 +5,20 @@ const userSchema = new Schema(
   {
     email: {
       type: String,
-      required: [true, 'Email is required.'],
+      required: [true, "Email is required."],
       unique: true,
       lowercase: true,
-      trim: true
+      trim: true,
     },
     password: {
       type: String,
-      required: [true, 'Password is required.']
-    }
+      required: [true, "Password is required."],
+    },
+    name: { type: String, required: true },
   },
   {
-    // this second object adds extra properties: `createdAt` and `updatedAt`    
-    timestamps: true
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
   }
 );
 
